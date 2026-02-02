417473b860574cc9882cd73978ed423059767188
Vivek Roy <vr5655881@gmail.com>
2026-02-02 23:19:10 +0530

    Add Customer entity

---
+++ Customer.java
@@ -0,0 +1,12 @@
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+}
